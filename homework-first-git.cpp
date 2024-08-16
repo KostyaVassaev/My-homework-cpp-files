5b312094ba7700045789ac45df63ba7c2cbc9fce
KostyaVassaev <111956596+KostyaVassaev@users.noreply.github.com>
2024-08-16 12:01:33 +0300

    Add files via upload

---
+++ homework-first-git.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n;
+    std::cout << "Enter the height of the triangle: ";
+    std::cin >> n;
+    std::cout << std::endl;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n - i; ++j)
+        {
+            std::cout << " ";
+        }
+
+        for (int k = 1; k <= (2 * i - 1); ++k)
+        {
+            std::cout << "*";
+        }
+
+        std::cout << std::endl;
+    }
+}
